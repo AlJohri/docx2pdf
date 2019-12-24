10d102ca6920de06c2108d21458b9edccd5bb5b8
Al Johri <al.johri@gmail.com>
2019-12-23 23:53:45 -0500

    initial commit

---
+++ docx2pdf/__init__.py
@@ -0,0 +1,113 @@
+import sys
+import pathlib
+import subprocess
+from importlib_metadata import version
+
+__version__ = version(__package__)
+
+"""
+TODO: make output not required, convert inplace
+TODO: move the dir/folder checks into python and add a batch argument?
+TODO: create for loop for windows batch convert
+"""
+
+def windows(input_path, output_path, keep_active):
+    import win32com.client
+    word = win32com.client.Dispatch('Word.Application')
+    wdFormatPDF = 17
+    docx_filepath = Path(input_path).resolve()
+    pdf_filepath = Path(output_path).resolve()
+    doc = word.Documents.Open(str(docx_filepath))
+    doc.SaveAs(str(pdf_filepath), FileFormat=wdFormatPDF)
+    doc.Close()
+    if not keep_active:
+    	word.Quit()
+
+def macos(input_path, output_path, keep_active):
+	paths = resolve_paths(input_path, output_path)
+	print(paths)
+	script = (pathlib.Path(__file__).parent / 'convert.jxa').resolve()
+	cmd = [
+		"/usr/bin/osascript",
+		"-l",
+		"JavaScript",
+		script,
+		paths['input'],
+		paths['output'],
+		str(keep_active).lower(),
+	]
+	return subprocess.run(cmd, check=True)
+
+def resolve_paths(input_path, output_path):
+	input_path = pathlib.Path(input_path).resolve()
+	output_path = pathlib.Path(output_path).resolve() if output_path else None
+	output = {}
+	if input_path.is_dir():
+		output['batch'] = True
+		output['input'] = str(input_path)
+		if output_path:
+			assert output_path.is_dir()
+		else:
+			output_path = str(input_path)
+		output['output'] = output_path
+	else:
+		output['batch'] = False
+		assert str(input_path).endswith('.docx')
+		output['input'] = str(input_path)
+		if output_path and output_path.is_dir():
+			output_path = str(output_path / (str(input_path.stem) + ".pdf"))
+		elif output_path:
+			assert str(output_path).endswith('.pdf')
+		else:
+			output_path = str(input_path.parent / (str(input_path.stem) + ".pdf"))
+		output['output'] = output_path
+	return output
+
+def convert(input_path, output_path, keep_active=False):
+	if sys.platform == "darwin":
+		return macos(input_path, output_path, keep_active)
+	else:
+		return windows(input_path, output_path, keep_active)
+
+def cli():
+
+	import textwrap
+	import argparse
+
+	if "--version" in sys.argv:
+		print(__version__)
+		exit(0)
+
+	description = textwrap.dedent("""
+	Example Usage:
+
+	Convert single docx file in-place from myfile.docx to myfile.pdf:
+		docx2pdf myfile.docx
+
+	Batch convert docx folder in-place. Output PDFs will go in the same folder:
+		docx2pdf myfolder/
+
+	Convert single docx file with explicit output filepath:
+		docx2pdf input.docx output.docx
+
+	Convert single docx file and output to a different explicit folder:
+		docx2pdf input.docx output_dir/
+
+	Batch convert docx folder. Output PDFs will go to a different explicit folder:
+		docx2pdf input_dir/ output_dir/
+	""")
+
+	formatter_class = lambda prog: argparse.RawDescriptionHelpFormatter(prog, max_help_position=32)
+	parser = argparse.ArgumentParser(description=description, formatter_class=formatter_class)
+	parser.add_argument('input', help='input file or folder. batch converts entire folder or convert single file')
+	parser.add_argument('output', nargs='?', help='output file or folder')
+	parser.add_argument('--keep-active', action='store_true', default=False, help='prevent closing word after conversion')
+	parser.add_argument('--version', action='store_true', default=False, help='display version and exit')
+
+	if len(sys.argv) == 1:
+		parser.print_help()
+		exit(0)
+	else:
+		args = parser.parse_args()
+
+	convert(args.input, args.output, args.keep_active)	
