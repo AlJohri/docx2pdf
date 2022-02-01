323b02456668980b13c9aecf7f1d131fa67c105c
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2022-02-01 18:14:41 +0000

    Tests checking that AssertionError is raised for incorrect extensions

--- tests/test_docx2pdf.py
+++ tests/test_docx2pdf.py
@@ -3,12 +3,28 @@ import sys
 import unittest
 from os.path import join
 from tempfile import TemporaryDirectory
+from unittest.mock import patch
 
 from docx2pdf import __version__, convert
 
 
 def test_version():
     assert __version__ == '0.1.8'
+
+
+class TestFailureModes(unittest.TestCase):
+    def test_input_not_docx_raises_assertion_error(self):
+        input_path = "file.not_docx"
+        with self.assertRaises(AssertionError):
+            convert(input_path)
+
+    @patch('docx2pdf.windows')
+    @patch('docx2pdf.macos')
+    def test_output_not_pdf_raises_assertion_error(self, m_macos, m_windows):
+        input_path = "file.docx"
+        output_path = "file.not_pdf"
+        with self.assertRaises(AssertionError):
+            convert(input_path, output_path)
 
 
 @unittest.skipIf(sys.platform != 'darwin', 'MacOS-only tests')
